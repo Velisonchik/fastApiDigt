aca4acf08aed9b894e13b0888f1ac2687b277fea
velisonchik <velisonchik@gmail.com>
2022-10-18 13:29:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import logging
+from fastapi import FastAPI
+from config import *
+
+app = FastAPI()
+logging.basicConfig(filename="errors.log", filemode="w", level=logging.ERROR, encoding='utf-8')
+
+
+def get_type(fn):
+    if os.path.isdir(fn):
+        return "folder"
+    elif os.path.isfile(fn):
+        return "file"
+    else:
+        return None
+
+
+async def check_path():
+    data = []
+    for i in os.listdir(defaul_path):
+        fully_path = defaul_path+i
+        try:
+            temp = {
+                "name": i,
+                "type": get_type(fully_path),
+                "time": int(os.path.getctime(fully_path))
+            }
+            data.append(temp)
+        except Exception as e:
+            logging.error(e)
+    if len(data) > 0:
+        return {"data": data}
+    else:
+        return {"data": "Read errors.log"}
+
+
+@app.get("/api/meta")
+async def show_data():
+    return await check_path()
